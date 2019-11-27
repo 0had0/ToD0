1a6f679634ec4f5ea1091306692c44b11ae580c7
0had0 <hadihoussainy75@gmail.com>
2019-11-28 01:13:16 +0200

    Updates

---
+++ precache-manifest.3f2b4463dbcc217f5ba1067cfe93f1e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2dd61164328be9ab8d59aaa13082a2",
+    "url": "/ToD0/index.html"
+  },
+  {
+    "revision": "891c936d4c3d41eb5956",
+    "url": "/ToD0/static/css/main.85a49355.chunk.css"
+  },
+  {
+    "revision": "a21ae2448d622cf2617d",
+    "url": "/ToD0/static/js/2.34734b61.chunk.js"
+  },
+  {
+    "revision": "891c936d4c3d41eb5956",
+    "url": "/ToD0/static/js/main.6b096e41.chunk.js"
+  },
+  {
+    "revision": "3963d24b496cfb4f2196",
+    "url": "/ToD0/static/js/runtime-main.4fb0b032.js"
+  }
+]);
