deb2f1bc719960e915bd8ce58fa6d60fa0e9245d
0had0 <hadihoussainy75@gmail.com>
2019-11-28 12:30:42 +0200

    prettier

--- src/components/show.js
+++ src/components/show.js
@@ -1,113 +1,117 @@
-import React, {useState} from 'react';
-import Container from '@material-ui/core/Container';
-import {makeStyles} from '@material-ui/core/styles';
-import uuidv1 from 'uuid/v1';
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import uuidv1 from "uuid/v1";
 
-import ToDo from './todo';
-import {addTodo, delTodo, editTodo} from '../actions/index';
-import {
-    TextField,
-    Button,
-    Box,
-    List
-} from '@material-ui/core';
+import ToDo from "./todo";
+import { addTodo, delTodo, editTodo } from "../actions/index";
+import { TextField, Button, Box, List } from "@material-ui/core";
 
 const Show = props => {
-    const [elm, setElm] = useState('');
-    const [history, setHistory] = useState([]);
+  const [elm, setElm] = useState("");
+  const [history, setHistory] = useState([]);
 
-    const store = props.store ;
+  const store = props.store;
 
-    function componentDidMount() {
-        store.subscribe(() => console.log(props.store.getState()));
+  function componentDidMount() {
+    store.subscribe(() => console.log(props.store.getState()));
+  }
+
+  const useStyless = makeStyles(theme => ({
+    root: {
+      width: "100%",
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper
+    },
+    padd: {
+      padding: theme.spacing(3, 2),
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignItems: "center"
     }
+  }));
 
+  const classes = useStyless();
 
-
-
-    const useStyless = makeStyles(theme => ({
-        root: {
-          width: '100%',
-          maxWidth: 360,
-          backgroundColor: theme.palette.background.paper,
-        },
-        padd: {
-            padding: theme.spacing(3, 2),
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            alignItems: 'center'
-        }
-    }));
-
-    const classes = useStyless();
-
-    const handleDel = item => {
-        console.log("Remove button is clicked")
-        console.log(`id: ${item.id}`)
-        setHistory([...history, {operation:"Delete", item}])
-        props.store.dispatch(delTodo(item.id));
-    }
-    const handleEdit = item => {
-        store.dispatch(editTodo(item));
-        setHistory([...history, {operation:"Edit", item}])
-    }
-    const handleAdd = () => {
-        console.log("Add button Clicked")
-        props.store.dispatch(addTodo({id: uuidv1(), content: elm, visiblity: true}));
-        setElm('')
-                            }
-    const handleChange = event => {
-        console.log("change is affected")
-        setElm(event.target.value);
-    }
-    return (
-        <React.Fragment>
-            <Container className={
-                classes.padd
-            }>
-                <Box position='static' display='flex' flexDirection='row' alignItems='flex-end'>
-                    <TextField id="normal-textarea" label="ToDo ..." placeholder="Memorizable thing ;)"
-                        value={elm}
-                        margin="normal"
-                        onChange={
-                            handleChange
-                        } required/>
-                      <Button size="small" color='primary'
-                        onClick={
-                            handleAdd
-                    }>
-                        Add
-                    </Button>
-                      <Button size="small" color='primary'
-                        onClick={
-                            () => {console.log(props.store.getState())}
-                    }>
-                        Store
-                    </Button>
-                      <Button size="small" color='primary'
-                        onClick={
-                            () => {console.log(history)}
-                    }>
-                        History
-                    </Button>
-                </Box>
-                <List className={classes.root}>
-                {
-                store.getState().map((item, i) => {
-                    return (
-                        <ToDo item={item}
-                            key={i}
-                            id={item.id}
-                            isEditing={false}
-                            handleEdit={handleEdit}
-                            handleDel={handleDel}/>
-                    )
-                })
-            } </List>
-          </Container>
-        </React.Fragment>
+  const handleDel = item => {
+    console.log("Remove button is clicked");
+    console.log(`id: ${item.id}`);
+    setHistory([...history, { operation: "Delete", item }]);
+    props.store.dispatch(delTodo(item.id));
+  };
+  const handleEdit = item => {
+    store.dispatch(editTodo(item));
+    setHistory([...history, { operation: "Edit", item }]);
+  };
+  const handleAdd = () => {
+    console.log("Add button Clicked");
+    props.store.dispatch(
+      addTodo({ id: uuidv1(), content: elm, visiblity: true })
     );
-}
+    setElm("");
+  };
+  const handleChange = event => {
+    console.log("change is affected");
+    setElm(event.target.value);
+  };
+  return (
+    <React.Fragment>
+      <Container className={classes.padd}>
+        <Box
+          position="static"
+          display="flex"
+          flexDirection="row"
+          alignItems="flex-end"
+        >
+          <TextField
+            id="normal-textarea"
+            label="ToDo ..."
+            placeholder="Memorizable thing ;)"
+            value={elm}
+            margin="normal"
+            onChange={handleChange}
+            required
+          />
+          <Button size="small" color="primary" onClick={handleAdd}>
+            Add
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => {
+              console.log(props.store.getState());
+            }}
+          >
+            Store
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => {
+              console.log(history);
+            }}
+          >
+            History
+          </Button>
+        </Box>
+        <List className={classes.root}>
+          {store.getState().map((item, i) => {
+            return (
+              <ToDo
+                item={item}
+                key={i}
+                id={item.id}
+                isEditing={false}
+                handleEdit={handleEdit}
+                handleDel={handleDel}
+              />
+            );
+          })}{" "}
+        </List>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default Show;
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,118 +1,124 @@
-import React from 'react';
+import React from "react";
 
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import {
-    Box,
-    Button,
-    TextField,
-    ListItemSecondaryAction,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Checkbox,
-    ButtonGroup
-  } from '@material-ui/core';
+  Box,
+  Button,
+  TextField,
+  ListItemSecondaryAction,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Checkbox,
+  ButtonGroup
+} from "@material-ui/core";
 
 const useStyless = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-        backgroundColor: '#607d8b',
-        color: 'white',
-        marginTop: '10px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        minWidth: '500px'
-    }
+  root: {
+    padding: theme.spacing(3, 2),
+    backgroundColor: "#607d8b",
+    color: "white",
+    marginTop: "10px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    minWidth: "500px"
+  }
 }));
 
 const Content = props => {
-    const [cont, setCont] = React.useState(props.item.content);
+  const [cont, setCont] = React.useState(props.item.content);
 
-    const handleChange = event => {
-        setCont(event.target.value)
-    }
-    const handleDone = () => {
-        //props.item.content = cont;
-        props.handleEdit(Object.assign({}, props.item, {content:cont}))
-        props.setEdit(false)
-    }
-    const handleCancel = () => {
-        props.setEdit(false)
-}
-    return (
-        <React.Fragment>
-        <TextField id="normal-textarea" placeholder="Can't be Empty"
-            value={
-                cont
-            }
-            margin="normal"
-            onChange={handleChange} required/>
-<Button onClick={handleDone} color='inherit'>Done</Button>
-<Button onClick={handleCancel} color='inherit'>Cancel</Button>
-        </React.Fragment>
-    );
-}
+  const handleChange = event => {
+    setCont(event.target.value);
+  };
+  const handleDone = () => {
+    //props.item.content = cont;
+    props.handleEdit(Object.assign({}, props.item, { content: cont }));
+    props.setEdit(false);
+  };
+  const handleCancel = () => {
+    props.setEdit(false);
+  };
+  return (
+    <React.Fragment>
+      <TextField
+        id="normal-textarea"
+        placeholder="Can't be Empty"
+        value={cont}
+        margin="normal"
+        onChange={handleChange}
+        required
+      />
+      <Button onClick={handleDone} color="inherit">
+        Done
+      </Button>
+      <Button onClick={handleCancel} color="inherit">
+        Cancel
+      </Button>
+    </React.Fragment>
+  );
+};
 
 const Cart = props => {
-    const [edit, setEdit] = React.useState(props.isEditing);
+  const [edit, setEdit] = React.useState(props.isEditing);
 
-    const handleClick = () => {
-        props.handleDel(props.item)
-    }
-    const handleEdit = () => {
-        setEdit(true);
-    }
+  const handleClick = () => {
+    props.handleDel(props.item);
+  };
+  const handleEdit = () => {
+    setEdit(true);
+  };
 
-    const [checked, setChecked] = React.useState([0]);
+  const [checked, setChecked] = React.useState([0]);
 
-    const handleToggle = value => () => {
+  const handleToggle = value => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
-   if (currentIndex === -1) {
-     newChecked.push(value);
-   } else {
-     newChecked.splice(currentIndex, 1);
-   }
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
 
-   setChecked(newChecked);
- };
-    return (
-      <ListItem role={undefined} dense button >
-
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                tabIndex={-1}
-                disableRipple
-              />
-            </ListItemIcon>
-            {
-            (edit) ? (
-                <Content item={props.item} setEdit={setEdit} handleEdit={props.handleEdit}/>
-            ) : (
-                <>
-                <ListItemText primary={props.item.content} />
-                <ButtonGroup
-              variant="text"
-              color="primary"
-              aria-label="full-width contained primary button group"
-            >
-            <Button onClick={handleEdit} color="primary">Edit</Button>
-          <Button onClick={handleClick} color="primary">Remove</Button>
+    setChecked(newChecked);
+  };
+  return (
+    <ListItem role={undefined} dense button>
+      <ListItemIcon>
+        <Checkbox edge="start" tabIndex={-1} disableRipple />
+      </ListItemIcon>
+      {edit ? (
+        <Content
+          item={props.item}
+          setEdit={setEdit}
+          handleEdit={props.handleEdit}
+        />
+      ) : (
+        <>
+          <ListItemText primary={props.item.content} />
+          <ButtonGroup
+            variant="text"
+            color="primary"
+            aria-label="full-width contained primary button group"
+          >
+            <Button onClick={handleEdit} color="primary">
+              Edit
+            </Button>
+            <Button onClick={handleClick} color="primary">
+              Remove
+            </Button>
           </ButtonGroup>
-                {/* <ListItemSecondaryAction>
+          {/* <ListItemSecondaryAction>
                   <Button onClick={handleEdit} color="primary">Edit</Button>
                 <Button onClick={handleClick} color="primary">Remove</Button>
                 </ListItemSecondaryAction> */}
-                </>
-            )
-          }
-          </ListItem>
-
-    );
-}
+        </>
+      )}
+    </ListItem>
+  );
+};
 
 export default Cart;
